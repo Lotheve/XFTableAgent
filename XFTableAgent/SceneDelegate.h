15b6d733b259d0759f65b3f095778803b05b6875
Lotheve <gaofeng-7171@163.com>
2020-01-06 16:18:22 +0800

    Initial Commit

---
+++ XFTableAgent/SceneDelegate.h
@@ -0,0 +1,16 @@
+//
+//  SceneDelegate.h
+//  XFTableAgent
+//
+//  Created by 卢旭峰 on 2020/1/6.
+//  Copyright © 2020 Lotheve. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SceneDelegate : UIResponder <UIWindowSceneDelegate>
+
+@property (strong, nonatomic) UIWindow * window;
+
+@end
+
